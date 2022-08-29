61e7fe685f629e60f1f4f04163c91d795f266364
flaviobartolo <flavio.bartolo2@gmail.com>
2022-08-30 00:33:26 +0100

    adding get place by id route

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,9 +2,37 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/', (req, res, next) => {
-  console.log('GET request in places')
-  res.json({message: 'It works'})
+const DUMMY_PLACES = [{
+  id: 'p1',
+  title: 'Empire State Building',
+  description: 'One of the most famous sky scrapers in the world!',
+  location: {
+    lat: 40.7484405,
+    lng: -73.9856644
+  },
+  address: '20 W 34th St., New York, NY 10001',
+  creator: 'u1'
+},
+{
+  id: 'p2',
+  title: 'Empire State Building 2',
+  description: 'One of the most famous sky scrapers in the world!',
+  location: {
+    lat: 40.7484405,
+    lng: -73.9856644
+  },
+  address: '20 W 34th St., New York, NY 10001',
+  creator: 'u2'
+}]
+
+router.get('/:pid', (req, res, next) => {
+  const placeId = req.params.pid
+  
+  place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId
+  })
+
+  res.json({place})
 })
 
 module.exports = router
